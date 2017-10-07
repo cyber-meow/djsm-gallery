e39d489049c07bc09bc974ae2ce5f899787e3a15
cyber-meow <sjungle305@gmail.com>
2017-10-07 18:56:25 +0200

    renew once all

---
+++ app.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cache from 'memory-cache';
+import middleware from './lib/middleware';
+
+const mongoDB = 'mongodb://localhost/doujinshi-manager';
+mongoose.connect(mongoDB, {
+  useMongoClient: true,
+});
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const ConfigSchema = new mongoose.Schema({
+  name: String,
+  staticRoot: String,
+});
+
+const Config = mongoose.model('Config', ConfigSchema);
+
+// Set staticRoot
+Config.findOne({ name: 'main' }, (err, config) => {
+  if (err) throw err;
+  if (!config) {
+    Config.create({
+      name: 'main',
+      staticRoot: '/media/yu-guan/DATA/pictures',
+    });
+    cache.put('staticRoot', '/media/yu-guan/DATA/pictures');
+  } else {
+    cache.put('staticRoot', config.staticRoot);
+  }
+});
+
+// Usage example with ExpressJS
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000;
+const host = process.env.OPENSHIFT_NODE || 'localhost';
+
+const app = express();
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/public`));
+
+app.use(middleware);
+
+app.listen(port, host);
+console.log(`node-gallery listening on ${host}:${port}`);
